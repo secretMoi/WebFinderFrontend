bf7499b504d4d42407536fcfd7c2b01839a443fd
Quentin <stagiaire63@rtl.be>
2022-01-28 23:27:00 +0100

    Add price ordering

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,11 +11,38 @@ export class AppComponent {
   @ViewChild('productToSearchInput', { static: false }) productToSearchInputReference!: ElementRef;
 
   products: Product[] = [];
+  isPricerOrderAscending: boolean = true;
 
   constructor(private httpService: HttpService) {}
 
   searchProducts() {
     this.httpService.searchProducts(this.productToSearchInputReference.nativeElement.value)
       .subscribe((products: Product[]) => this.products = products);
+  }
+
+  togglePriceOrder() {
+    this.isPricerOrderAscending = !this.isPricerOrderAscending;
+
+    this.orderProducts();
+  }
+
+  orderProducts(){
+    if(!this.isPricerOrderAscending) {
+      this.products.sort((product1, product2) => {
+        if(product1.price && product2.price) {
+          return product1.price < product2.price ? 1 : -1
+        }
+        return -1;
+      });
+    }
+
+    else if(this.isPricerOrderAscending) {
+      this.products.sort((product1, product2) => {
+        if(product1.price && product2.price) {
+          return product1.price > product2.price ? 1 : -1
+        }
+        return -1;
+      });
+    }
   }
 }
