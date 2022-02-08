dbe8c844cea274b85eabf9fdf5350486feddf7b5
secretMoi <win.moi@hotmail.com>
2022-02-08 22:02:50 +0100

    Add monitor option on product

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {ProductService} from "../service/product.service";
+import {Product} from "../model/product.model";
+import {HttpService} from "../service/http.service";
 
 @Component({
   selector: 'app-product-list',
@@ -8,9 +10,15 @@ import {ProductService} from "../service/product.service";
 })
 export class ProductListComponent implements OnInit {
 
-  constructor(public productService: ProductService) { }
+  constructor(
+    public productService: ProductService,
+    private httpService: HttpService,
+  ) { }
 
   ngOnInit(): void {
   }
 
+  addMonitor(product: Product) {
+
+  }
 }
